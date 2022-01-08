715260f1112806267fd1c6321c938b31b60178e1
Hongcheng <lihongchengsx@outlook.com>
2022-01-08 23:51:02 +0800

    upload buffer wrapper

---
+++ Humpback/D3DUtil.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <windows.h>
+#include <wrl.h>
+#include <dxgi1_4.h>
+#include <d3d12.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <cstdint>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+#include "d3dx12.h"
+
+#include "HumpbackHelper.h"
+
+
+
+namespace Humpback {
+
+class D3DUtil 
+{
+public:
+	static unsigned int CalConstantBufferByteSize(unsigned int byteSize)
+	{
+        // Constant buffers must be a multiple of the minimum hardware
+        // allocation size (usually 256 bytes).  So round up to nearest
+        // multiple of 256.  We do this by adding 255 and then masking off
+        // the lower 2 bytes which store all bits < 256.
+        // Example: Suppose byteSize = 300.
+        // (300 + 255) & ~255
+        // 555 & ~255
+        // 0x022B & ~0x00ff
+        // 0x022B & 0xff00
+        // 0x0200
+        // 512
+		return (byteSize + 255) & ~255;
+	}
+};
+
+}
+
